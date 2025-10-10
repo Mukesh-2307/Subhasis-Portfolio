090fbf1daacd70c0206d96cb9fe73c73f9c8dab1
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-10-10 23:53:49 +0530

    update: adding home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <>
+      <div id="hero-section">
+        <div className='w-[calc(100vw-180px-290px)] h-[calc(100vh-140px-128px)] justify-self-center flex justify-center items-center'>
+          <div>
+            <h1>Hi I’m subhasis, a Software Engineer</h1>
+            <p>I am an engineer with a passion for automation, data, and smart systems. I’ve built tools that streamline operations and boost efficiency. Skilled in Python, SQL, and front-end tech, I enjoy turning complex problems into simple, scalable solutions.</p>
+          </div>
+        </div>
+      </div>
+      <div >
+
+      </div>
+    </>
+  )
+}
+
+export default Home
