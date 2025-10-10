0d1eb5f9b1cd04feea41d0215d2919ae36d21f3b
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-10-11 00:27:57 +0530

    update: added text-font

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
       <div id="hero-section">
         <div className='w-[calc(100vw-180px-290px)] h-[calc(100vh-140px-128px)] justify-self-center flex justify-center items-center'>
           <div>
-            <h1>Hi I’m subhasis, a Software Engineer</h1>
-            <p>I am an engineer with a passion for automation, data, and smart systems. I’ve built tools that streamline operations and boost efficiency. Skilled in Python, SQL, and front-end tech, I enjoy turning complex problems into simple, scalable solutions.</p>
+            <h1>Hi I’m subhasis,<br />a Software Engineer</h1>
+            <p className='text-[var(--color-light-gray)]'>I am an engineer with a passion for automation, data, and smart systems.<br />I’ve built tools that streamline operations and boost efficiency.<br />Skilled in Python, SQL, and front-end tech, I enjoy turning complex problems into simple, scalable solutions.</p>
           </div>
         </div>
       </div>
