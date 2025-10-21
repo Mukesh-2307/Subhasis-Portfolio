6a59d53dc2cd121fb35c250c08929c38c7b2f483
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-10-21 13:14:22 +0530

    update: adding different sections

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import './Home.css'
 import { FaXTwitter, FaInstagram, FaGithub, FaLinkedinIn } from "react-icons/fa6";
+import SectionTemplate from '../../components/sectionTemplate/SectionTemplate';
+import TechStack from '../../components/TechStack';
+
+import { BsFillCpuFill } from "react-icons/bs";
+import { HiBriefcase } from "react-icons/hi2";
+import { TbDeviceDesktopCode } from "react-icons/tb";
+import { RiGitRepositoryFill } from "react-icons/ri";
+
+
+import ExperienceList from '../../components/ExperienceList';
+import Footer from '../../components/Footer/Footer';
 
 const socialsLink = [
   { label: 'twitter', link: '#' },
@@ -15,7 +26,7 @@ const Home = () => {
       <div id="hero-section">
         <div className='w-[calc(100vw-180px-290px)] h-[calc(100vh-140px-128px)] justify-self-center flex justify-center items-center'>
           <div>
-            <h1 className='text-[72px]/[86px]'>Hi I’m subhasis,<br />a Software Engineer</h1>
+            <h1 className='text-[80px]/[94px]'>Hi I’m subhasis,<br />a Software Engineer</h1>
             <p className='text-[var(--color-dark-gray)]'>I am an engineer with a passion for automation, data, and smart systems.<br />I’ve built tools that streamline operations and boost efficiency.<br />Skilled in Python, SQL, and front-end tech, I enjoy turning complex problems into simple, scalable solutions.</p>
           </div>
         </div>
@@ -45,11 +56,19 @@ const Home = () => {
           </div>
         </div>
 
-        <div className='flex items-center justify-between h-[calc(100vh-140px-650px)] my-16'>
+        <div className='flex items-start justify-between my-16'>
           <p className='title w-[840px]'>curious and driven individual who thrives on solving problems and learning new things.</p>
           <p className='title-s w-[440px] text-right'>What sets me apart is my ability to blend technical expertise with product and operational insight</p>
         </div>
+
       </div>
+      <SectionTemplate title="Tech Stack" subText="I bring automation and data-driven solutions that boost efficiency, scalability, and impact for growing teams and businesses" MyComponent={TechStack} MyIconComponent={BsFillCpuFill} loadMore={false} colorScheme="--color-purple"/>
+
+      <SectionTemplate title="Experience" subText="I have practical experience driving operational efficiency through automation, data pipelines, and product solutions in fast-paced, real-world environments" MyComponent={ExperienceList} MyIconComponent={HiBriefcase} loadMore={true} colorScheme="--color-pink"/>
+
+      <SectionTemplate title="Projects" subText="I’ve built impactful projects across automation, data pipelines, and IoT, showcasing my ability to deliver real-world tech solutions" MyComponent={ExperienceList} MyIconComponent={RiGitRepositoryFill} loadMore={true} colorScheme="--color-green"/>
+
+      <Footer/>
     </>
   )
 }
