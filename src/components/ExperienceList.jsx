d9713bd674af85a47b84e20d4b4a16c2d1edc51e
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-10-29 00:07:17 +0530

    update: populated experience section using experience list component

--- src/components/ExperienceList.jsx
+++ src/components/ExperienceList.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 
+const expList = [
+  {
+    company: "Flipkart",
+    start: "Nov 2023",
+    end: 'now',
+    position: "Supply Chain Ops Analyst Trainee"
+  },
+  {
+    company: "BSNL",
+    start: "May 2022",
+    end: 'June 2022',
+    position: "Networking Intern"
+  }
+]
+
 const ExperienceList = () => {
   return (
-    <div>ExperienceList</div>
+    <>
+      {expList?.map((exp, index) => {
+        return (
+          <div className='flex flex-col text-[50px]/[56px] my-16 pb-10 border-b-[0.5px] border-[var(--color-light-gray)] gap-10' key={index}>
+            <div className='flex justify-between items-center'>
+              <p>{exp.company}</p>
+              <p>{exp.position}</p>
+            </div>
+            <p>{exp.start} - {exp.end}</p>
+          </div>
+        )
+      })}
+    </>
   )
 }
 
