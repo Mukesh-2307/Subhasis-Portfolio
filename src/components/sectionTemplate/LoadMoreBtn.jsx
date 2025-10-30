a9c8fcb6d29067e8383a7f86be6d6c86a1b2a9f8
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-10-31 00:03:39 +0530

    update: adding path to load more btn

--- src/components/sectionTemplate/LoadMoreBtn.jsx
+++ src/components/sectionTemplate/LoadMoreBtn.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { HiArrowSmRight } from "react-icons/hi";
 
 
-const LoadMoreBtn = ({colorScheme}) => {
+const LoadMoreBtn = ({ colorScheme, redirectTo }) => {
     return (
-        <button className='flex gap-4 justify-between items-center border-[0.5px] border-[var(--color-light-gray)] rounded-full p-4 cursor-pointer self-center'>
-            Load More
-            <div className='size-12 bg-[var(--color-creame)] rounded-full flex justify-center items-center text-black'>
-                <HiArrowSmRight className='size-6' style={{color: `var(${colorScheme})`}}/>
-            </div>
+        <button className='border-[0.5px] border-[var(--color-light-gray)] rounded-full p-4 cursor-pointer self-center'>
+            <a href={redirectTo} className='flex gap-4 justify-between items-center '>
+                Load More
+                <div className='size-12 bg-[var(--color-creame)] rounded-full flex justify-center items-center text-black'>
+                    <HiArrowSmRight className='size-6' style={{ color: `var(${colorScheme})` }} />
+                </div>
+            </a>
         </button>
     )
 }
