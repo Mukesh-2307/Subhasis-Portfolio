de1dcd463ee317ccee2ceae292474007bf01011e
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-10-30 23:41:36 +0530

    update: added icons to footer items

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaLinkedin, FaSquareInstagram, FaSquareGithub, FaSquareFacebook, FaSquareXTwitter, FaSquareYoutube } from "react-icons/fa6";
 
 const Footer = () => {
     return (
@@ -10,23 +11,41 @@ const Footer = () => {
                         <p className='text-(--color-red)'>Explore</p>
                         <p className='col-span-2 text-(--color-purple)'>Follow Me</p>
                         <a href="#">Home</a>
-                        <a href="#">LinkedIn</a>
-                        <a href="#">Instagram</a>
+                        <div className='flex gap-4 items-center justify-start'>
+                            <FaLinkedin className='size-6'/>
+                            <a href="#">LinkedIn</a>
+                        </div>
+                        <div className='flex gap-4 items-center justify-start'>
+                            <FaSquareInstagram className='size-6'/>
+                            <a href="#">Instagram</a>
+                        </div>
                         <a href="#">Project</a>
-                        <a href="#">Github</a>
-                        <a href="#">Facebook</a>
+                        <div className='flex gap-4 items-center justify-start'>
+                            <FaSquareGithub className='size-6'/>
+                            <a href="#">Github</a>
+                        </div>
+                        <div className='flex gap-4 items-center justify-start'>
+                            <FaSquareFacebook className='size-6'/>
+                            <a href="#">Facebook</a>
+                        </div>
                         <a href="#">About Me</a>
-                        <a href="#">X</a>
-                        <a href="#">Youtube</a>
+                        <div className='flex gap-4 items-center justify-start'>
+                            <FaSquareXTwitter className='size-6'/>
+                            <a href="#">X</a>
+                        </div>
+                        <div className='flex gap-4 items-center justify-start'>
+                            <FaSquareYoutube className='size-6'/>
+                            <a href="#">Youtube</a>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className='flex justify-between items-center py-2'>
                 <p>subumohanty ©2025 - Privacy Policy</p>
-                <p>Inspired by <a href="https://www.thegr8binil.me/" style={{color: `var(--color-light-teal)`}}>thegr8binil.me</a></p>
+                <p>Inspired by <a href="https://www.thegr8binil.me/" style={{ color: `var(--color-light-teal)` }}>thegr8binil.me</a></p>
                 <p>Odisha, India</p>
             </div>
-        </div>
+        </div >
     )
 }
 
