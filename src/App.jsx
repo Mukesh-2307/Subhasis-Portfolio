d581668aef18463abb5e96aa95498721b7e7c7e1
MukeshDalai <mukeshdalai.2307@gmail.com>
2025-11-03 00:02:03 +0530

    update: adding custom framer motion components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
+import { AnimatePresence, motion } from 'framer-motion'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
+  const location = useLocation();
 
   return (
     <>
       <Navbar />
-      <Outlet />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.4, ease: [0.25, 0.25, 0.25, 0.75] }}
+        >
+          <Outlet />
+        </motion.div>
+      </AnimatePresence>
+      <ScrollToTop />
     </>
   )
 }
